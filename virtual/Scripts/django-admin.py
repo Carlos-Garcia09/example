54b2135cabeacbf57e41ed618d48c233fcdd7b1b
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-11-27 19:34:41 -0600

    Delete virtual directory

--- virtual/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\pc\desktop\nueva carpeta (11)\nueva carpeta\virtual\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
